31ba224f10e3931626f35e2f06bacf1b101d6f5d
kyongsuk lee <kyongsuk.lee@sk.com>
2019-06-26 10:44:00 +0900

    run spark in one container

--- intergration/spark-env.sh
+++
@@ -1,5 +0,0 @@
-export SPARK_MASTER_PORT=7080
-export SPARK_WORKER_WEBUI_PORT=8081
-export SPARK_HOME=/spark
-export SPARK_MASTER_HOST=$(ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
-export SPARK_LOCAL_IP=$(ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
--- intergration/start-all.sh
+++ intergration/start-all.sh
@@ -2,7 +2,8 @@
 #!/bin/bash
 
 export SPARK_HOME=/spark
-export SPARK_WORKER_WEBUI_PORT=8081
+#export SPARK_WORKER_WEBUI_PORT=8081
+#export SPARK_MASTER_HOST=`hostname`
 # turn on bash's job control
 set -m
 
@@ -11,15 +12,11 @@ set -m
 
 #echo $SPARK_CONF_DIR &
 # Start the primary process and put it in the background
- #/Users/eldorado0402/spark/spark-2.3.3-bin-hadoop2.7/sbin/start-master.sh &
-cd /${SPARK_HOME}/bin && /${SPARK_HOME}/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --ip 127.0.01 --port 7077 --webui-port 7080 &
- #./start-master.sh
+cd /${SPARK_HOME}/bin && /${SPARK_HOME}/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --host ${SPARK_MASTER_HOST} --port ${SPARK_MASTER_PORT} --webui-port ${SPARK_MASTER_WEBUI_PORT} &
 # Start the helper process
-#/Users/eldorado0402/spark/spark-2.3.3-bin-hadoop2.7/sbin/start-slave.sh spark://127.0.0.1:4000
-/${SPARK_HOME}/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port 8081  spark://127.0.0.1:7077
+/${SPARK_HOME}/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port ${SPARK_WORKER_WEBUI_PORT}  spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT}
 # the my_helper_process might need to know how to wait on the
 # primary process to start before it does its work and returns
-
 
 # now we bring the primary process back into the foreground
 # and leave it there
